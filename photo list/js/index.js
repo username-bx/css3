9bd4a551abb6c1d2f6775f15acaa5754dde77c56
hongkechuanmei <570916824@qq.com>
2021-07-08 10:54:56 +0800

    7月8号 photolist done

--- photo list/js/index.js
+++ photo list/js/index.js
@@ -7,5 +7,67 @@ timer = setTimeout(function () {
     ul.removeAttribute('class', 'init');
 }, 200)
 
+var last = null;
+lis.forEach(function (li, index) {
+    li.onclick = function () {
+
+        ul.setAttribute('class', 'menu')
+
+        last && (last.className = '');
+
+        this.className = 'active';
+
+        last = this;
+    }
+
+    closeBtn[index].onclick = function (e) {
+        e.cancelBubble = true;
+
+        ul.removeAttribute('class', 'menu');
+
+        li[index].removeAttribute('class', 'active');
+
+        last = null;
+    }
+});
 
 console.log(ul, lis, closeBtn);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function aaa() {
+//     return {
+//         test: 1
+//     }
+// }
+
+// console.log('第二题' + typeof aaa())
+
+
+// var c = document.getElementById('#only');
+// console.log(c);
+
+// var a = [1, 2, 3]
+// console.log('第四题' + typeof a);
